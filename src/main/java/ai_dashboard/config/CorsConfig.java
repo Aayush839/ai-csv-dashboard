d2b93adb2679b5ba698db461fd98f6a9c7623970
Aayush <aayushmaheshwari2103@gmail.com>
2026-04-19 14:05:55 +0530

    fix CORS for frontend

--- src/main/java/ai_dashboard/config/CorsConfig.java
+++ src/main/java/ai_dashboard/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://ai-csv-frontend.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
